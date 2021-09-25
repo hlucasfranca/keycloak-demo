439d8da5a2c9c88c27590f14facc28d854f36e42
Henrique Lucas França <hlucasfranca@gmail.com>
2021-09-25 15:58:54 -0300

    adiciona role default

--- spring-boot-keycloak/src/main/java/com/demo/keycloak/SecurityConfig.java
+++ spring-boot-keycloak/src/main/java/com/demo/keycloak/SecurityConfig.java
@@ -45,7 +45,7 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         super.configure(http);
         http.authorizeRequests()
             .antMatchers("/customers*", "/users*")
-            .hasRole("*")
+            .hasRole("offline_access")
             .anyRequest()
             .permitAll();
     }
