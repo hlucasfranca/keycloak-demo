b08eedc33567d59c19ec9e7309c56a7855beda8d
Henrique Lucas França <hlucasfranca@gmail.com>
2021-09-25 16:07:49 -0300

    autenticador usuario

--- auth-server/keycloak-custom-providers/src/main/java/com/demo/anthentication/authenticator/user/UserAuthenticator.java
+++ auth-server/keycloak-custom-providers/src/main/java/com/demo/anthentication/authenticator/user/UserAuthenticator.java
@@ -11,11 +11,11 @@ import org.slf4j.LoggerFactory;
 
 public class UserAuthenticator implements Authenticator {
 
-    private static final Logger logger = LoggerFactory.getLogger(CustomUserStorageProvider.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserAuthenticator.class);
 
     @Override
     public void authenticate(AuthenticationFlowContext context) {
-        logger.debug("authenticate called ... context = " + context);
+        logger.info("authenticate called ... context = " + context);
 
         String a = "";
         context.success();
@@ -23,34 +23,31 @@ public class UserAuthenticator implements Authenticator {
 
     @Override
     public void action(AuthenticationFlowContext context) {
-//        logger.info("autenticado com sucesso  " + context);
-//        String a = "";
-//        context.success();
     }
 
 
     @Override
     public boolean requiresUser() {
-        logger.debug("requiresUser called ... returning true");
+        logger.info("requiresUser called ... returning true");
         return true;
     }
 
     @Override
     public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
-        logger.debug("configuredFor called ... session=" + session + ", realm=" + realm + ", user=" + user);
+        logger.info("configuredFor called ... session=" + session + ", realm=" + realm + ", user=" + user);
         boolean result = true;
-        logger.debug("... returning " + result);
+        logger.info("... returning " + result);
         return result;
     }
 
     @Override
     public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
-        logger.debug("setRequiredActions called ... session=" + session + ", realm=" + realm + ", user=" + user);
+        logger.info("setRequiredActions called ... session=" + session + ", realm=" + realm + ", user=" + user);
     }
 
     @Override
     public void close() {
-        logger.debug("close called ...");
+        logger.info("close called ...");
     }
 
 }
--- auth-server/keycloak-custom-providers/src/main/java/com/demo/anthentication/authenticator/user/UserAuthenticatorFactory.java
+++ auth-server/keycloak-custom-providers/src/main/java/com/demo/anthentication/authenticator/user/UserAuthenticatorFactory.java
@@ -1,7 +1,6 @@
 package com.demo.anthentication.authenticator.user;
 
 import com.demo.anthentication.authenticator.sms.SMSAuthenticatorConstants;
-import org.jboss.logging.Logger;
 import org.keycloak.Config;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.AuthenticatorFactory;
@@ -9,6 +8,8 @@ import org.keycloak.models.AuthenticationExecutionModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.HttpMethod;
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ public class UserAuthenticatorFactory implements AuthenticatorFactory {
 
     public static final String PROVIDER_ID = "user-authentication";
 
-    private static Logger logger = Logger.getLogger(UserAuthenticatorFactory.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserAuthenticatorFactory.class);
     private static final UserAuthenticator SINGLETON = new UserAuthenticator();
     private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
             AuthenticationExecutionModel.Requirement.REQUIRED,
@@ -36,65 +37,65 @@ public class UserAuthenticatorFactory implements AuthenticatorFactory {
 
     @Override
     public String getId() {
-        logger.debug("getId called ... returning " + PROVIDER_ID);
+        logger.info("getId called ... returning " + PROVIDER_ID);
         return PROVIDER_ID;
     }
 
     @Override
     public Authenticator create(KeycloakSession session) {
-        logger.debug("create called ... returning " + SINGLETON);
+        logger.info("create called ... returning " + SINGLETON);
         return SINGLETON;
     }
 
 
     @Override
     public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
-        logger.debug("getRequirementChoices called ... returning " + REQUIREMENT_CHOICES);
+        logger.info("getRequirementChoices called ... returning " + REQUIREMENT_CHOICES);
         return REQUIREMENT_CHOICES;
     }
 
     @Override
     public boolean isUserSetupAllowed() {
-        logger.debug("isUserSetupAllowed called ... returning true");
+        logger.info("isUserSetupAllowed called ... returning true");
         return true;
     }
     @Override
     public boolean isConfigurable() {
         boolean result = true;
-        logger.debug("isConfigurable called ... returning " + result);
+        logger.info("isConfigurable called ... returning " + result);
         return result;
     }
     @Override
     public String getHelpText() {
-        logger.debug("getHelpText called ...");
+        logger.info("getHelpText called ...");
         return "Validates an OTP sent by SMS.";
     }
     @Override
     public String getDisplayType() {
         String result = "User Authentication";
-        logger.debug("getDisplayType called ... returning " + result);
+        logger.info("getDisplayType called ... returning " + result);
         return result;
     }
     @Override
     public String getReferenceCategory() {
-        logger.debug("getReferenceCategory called ... returning sms-auth-code");
+        logger.info("getReferenceCategory called ... returning sms-auth-code");
         return "usuario-auth";
     }
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
-        logger.debug("getConfigProperties called ... returning " + configProperties);
+        logger.info("getConfigProperties called ... returning " + configProperties);
         return configProperties;
     }
     @Override
     public void init(Config.Scope config) {
-        logger.debug("init called ... config.scope = " + config);
+        logger.info("init called ... config.scope = " + config);
     }
     @Override
     public void postInit(KeycloakSessionFactory factory) {
-        logger.debug("postInit called ... factory = " + factory);
+        logger.info("postInit called ... factory = " + factory);
     }
     @Override
     public void close() {
-        logger.debug("close called ...");
+        logger.info("close called ...");
     }
 }
